91ac62441589d69845163922caeda2acb478978e
SnehaChouksey <snehachoukseyobc@gmail.com>
2025-12-09 03:29:04 +0530

    UX improvements

--- client/app/quiz/page.tsx
+++ client/app/quiz/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
