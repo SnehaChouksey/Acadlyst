f65b149c3be3e95a9432481e7c0f0346d5614174
SnehaChouksey <snehachoukseyobc@gmail.com>
2025-12-09 03:18:55 +0530

    UX improvemments

--- client/app/summarizer/pdf/page.tsx
+++ client/app/summarizer/pdf/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
